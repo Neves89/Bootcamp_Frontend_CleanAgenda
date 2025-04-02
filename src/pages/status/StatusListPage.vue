dbce20f426816b480547ece7886294b621b32dc0
Neves89 <167469421+Neves89@users.noreply.github.com>
2025-04-01 22:14:14 -0300

    Primeiro Commit
    Estrutura do projeto

---
+++ src/pages/status/StatusListPage.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { DefaultTemplate } from '@/template'
+
+const itensDaTabela = [
+    {
+        nome: 'Elephante Africano',
+        species: 'Loxodonta africana',
+        diet: 'Herbivore',
+        habitat: 'Savannah',
+    },
+    {
+        nome: 'Africano',
+        species: 'Loxodonta africana',
+        diet: 'Herbivore',
+        habitat: 'Savannah',
+    },
+    {
+        nome: 'Africano',
+        species: 'Loxodonta africana',
+        diet: 'Herbivore',
+        habitat: 'Savannah',
+    },
+    {
+        nome: 'Africano',
+        species: 'Loxodonta africana',
+        diet: 'Herbivore',
+        habitat: 'Savannah',
+    },
+    
+]
+</script>
+
+<template>
+    <DefaultTemplate>
+        <v-data-table :items="itensDaTabela"></v-data-table>
+    </DefaultTemplate>
+</template>
---
+++ src/router/routes.ts
@@ -0,0 +1,13 @@
+export const routes = [
+  {
+    path: '/',
+    name: 'dashboard',
+    component: () =>
+      import(/* webpackChunkName: "dashboard" */ '@/pages/dashboard/DashboardPage.vue')
+  },
+  {
+    path: '/status',
+    name: 'status-list',
+    component: () => import(/* webpackChunkName: "status" */ '@/pages/status/StatusListPage.vue')
+  }
+]
